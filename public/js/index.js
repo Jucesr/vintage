21b07c59a122df0c31ab7a4fd24493c583a338b2
Julio Ojeda <jcom.94m@gmail.com>
2018-01-08 15:42:12 -0800

    Add contact page and footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,40 @@
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 $(window).scroll( function() {
   if(window.scrollY > 100 ){
     $("#header").addClass("header__background");
@@ -9,3 +46,33 @@ $(window).scroll( function() {
     $('#logo').width(70);
   }
 });
+
+$( "#date" ).datepicker({
+  monthNames: [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ],
+  dayNames: [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado"
+  ],
+  dayNamesMin: [ "D", "L", "M", "M", "J", "V", "S" ],
+  dateFormat: "dd MM yy"
+});
+
+$('#phone').mask('(000)000-0000', {placeholder: "Telefono (___)___-____"});
