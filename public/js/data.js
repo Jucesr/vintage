ce78f077bb0c7789092ee2f61cda2e008422f1b1
Julio Ojeda <jcom.94m@gmail.com>
2018-01-10 02:03:40 -0800

    Add services page and other things

---
+++ public/js/data.js
@@ -0,0 +1,241 @@
+var catalogs_data = [
+  {
+    "title": "Carrito de shots",
+    "packages":
+      [
+        {
+          "name": "Estándar",
+          "info": [
+            "140 Shots",
+            "4 Sabores a elegir",
+            "1 Bartender animador"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "210 Shots",
+            "5 Sabores a elegir",
+            "1 Bartender animador"
+          ]
+        },{
+          "name": "Gold",
+          "info": [
+            "350 Shots",
+            "6 Sabores a elegir",
+            "1 Bartender animador"
+          ]
+        },{
+          "name": "Plus",
+          "info": [
+            "490 Shots",
+            "7 Sabores a elegir",
+            "1 Bartender animador"
+          ]
+        },{
+          "name": "Vintage",
+          "info": [
+            "630 Shots",
+            "8 Sabores a elegir",
+            "2 Bartender animador",
+            "Mochila fumigadora"
+          ]
+        }
+      ]
+  },
+  {
+    "title": "Barra de cocteles",
+    "packages": [
+        {
+          "name": "Estándar",
+          "info": [
+            "100 Cocteles",
+            "Barra Iluminada",
+            "Bartender",
+            "3 Cocteles a elegir"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "200 Cocteles",
+            "Barra Iluminada",
+            "Bartender",
+            "5 Cocteles a elegir"
+          ]
+        },{
+          "name": "Vintage",
+          "info": [
+            "200 Cocteles",
+            "Barra Iluminada",
+            "Bartender animador",
+            "Mochila fumigadora",
+            "Shots de regalo",
+            "5 Cocteles a elegir"
+          ]
+        }
+    ],
+    "message": "Todos los paquetes incluyen: vasos, servilletas, bailarinas, popotes, sal, hielo, limón y lo necesario para la barra."
+
+  },
+  {
+    "title": "Barra de clamatos preparados",
+    "packages": [
+        {
+          "name": "Estándar",
+          "info": [
+            "Para 150 personas",
+            "Barra Iluminada",
+            "Bartender",
+            "2 horas"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "Para 200 personas",
+            "Barra Iluminada",
+            "Bartender",
+            "2 horas"
+          ]
+        },{
+          "name": "Vintage",
+          "info": [
+            "Para 250 personas",
+            "Barra Iluminada",
+            "Bartender",
+            "2 horas"
+          ]
+        }
+    ]
+
+  },
+  {
+    "title": "Mochila fumigadora",
+    "packages": [
+        {
+          "name": "Estándar",
+          "info": [
+            "3 Litros de bebida",
+            "1 Animador"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "7 Litros de bebida",
+            "1 Animador"
+          ]
+        },{
+          "name": "Vintage",
+          "info": [
+            "5 Litros de bebida",
+            "1 Animador",
+            "Show de brindis",
+            "Ruleta de shots"
+          ]
+        }
+    ]
+
+  },
+  {
+    "title": "Pharmacy bar",
+    "packages": [
+        {
+          "name": "Estándar",
+          "info": [
+            "100 Personas",
+            "Animador vestido de enfermo"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "150 Personas",
+            "Animador vestido de enfermo"
+          ]
+        },{
+          "name": "Vintage",
+          "info": [
+            "200 Personas",
+            "Animador vestido de enfermo"
+          ]
+        }
+    ],
+    "message": "Todos los paquetes incluyen: sueros, halls, mentas, aspirinas, saladitos, alka seltzer, shots levanta muertos, sodas, agua de coco, agua, chicles y mas."
+  },
+  {
+    "title": "Coffee break",
+    "packages": [
+        {
+          "name": "Sencillo",
+          "info": [
+            "Café y agua",
+            "Refrescos",
+            "Galletas",
+            "Dulces",
+            "Solo una puesta"
+          ]
+        },{
+          "name": "Continuo",
+          "info": [
+            "Café y agua",
+            "Refrescos",
+            "Galletas",
+            "Dulces",
+            "Por 5 horas"
+          ]
+        },{
+          "name": "Premium",
+          "info": [
+            "Café y agua",
+            "2 tipos de Te",
+            "Refrescos",
+            "Nestea embotellado",
+            "Galletas",
+            "Dulces"
+          ]
+        }
+    ],
+    "message": "Extras: Finger Sandwich, Fruta, Jugo, Granola, Miel, Canapes, Taquitos, Quesadillas, Chimichangas, Burritos de machaca."
+  },
+  {
+    "title": "Comida",
+    "packages": [
+        {
+          "name": "Taquiza",
+          "info": [
+            "Buffet",
+            "Carne asada",
+            "Pollo",
+            "Pastor",
+            "Tripa",
+            "Incluye: Salsas, Guacamole, Pepino, Rabanos, Repollo, Chiles, Cebollitas, Limones, Frijoles"
+          ]
+        },{
+          "name": "Hamburguesa",
+          "info": [
+            "Incluye: Papas fritas, Chiles en rajas, Toreados, Aderezo",
+          ]
+        },{
+          "name": "Trasnochador",
+          "info": [
+            "Burritos de machaca",
+            "Chilaquiles",
+            "Incluye: Platos, Servilletas y Cubiertos desechables",
+          ]
+        }
+    ]
+  },
+  {
+    "title": "Extras",
+    "packages": [
+        {
+          "name": "Aun mas!",
+          "info": [
+            "Capitan de meseros",
+            "Meseros de bebidas",
+            "Meseros de cena",
+            "Meseros de bebida y cena",
+            "Bartender",
+            "Hosstes"
+          ]
+        }
+    ]
+  }
+]
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+$.getJSON("./data.json", function(result){
+  alert('d');
+  console.log(result);
+        $.each(result, function(i, field){
+            $("div").append(field + " ");
+        });
+    });
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -17,20 +25,7 @@ $('a[href*="#"]')
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+        smoothScroll(target);
       }
     }
   });
@@ -76,3 +71,94 @@ $( "#date" ).datepicker({
 });
 
 $('#phone').mask('(000)000-0000', {placeholder: "Telefono (___)___-____"});
+
+$('#catalog0').hide();
+
+function smoothScroll(target){
+  $('html, body').animate({
+    scrollTop: target.offset().top
+  }, 1000, function() {
+    // Callback after animation
+    // Must change focus!
+    var $target = $(target);
+    $target.focus();
+    if ($target.is(":focus")) { // Checking if the target was focused
+      return false;
+    } else {
+      $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+      $target.focus(); // Set focus again
+    };
+  });
+}
+
+function show_catalog(catalog_id){
+  var catalog_info = $('#catalog_info');
+  catalog_info.html('');
+  //Create package section
+  var catalog = $('<div id="catalog" class="catalog__shots"></div>');
+    var catalog_title = $(`<h3>${catalogs_data[catalog_id].title}</h3>`);
+    var catalog_packages = $('<div class="catalog__packages">');
+
+  //Package items.
+  var i = 1;
+  catalogs_data[catalog_id].packages.map(function(package){
+    var package_div = $('<div class="package">');
+      var package_name = $(`<h4>${package.name}</h4>`);
+      //Exception for food and extras section.
+      var cat_extension ='';
+      switch (catalog_id) {
+        case 6:
+          cat_extension = 'f';
+        break;
+        case 7:
+          cat_extension = 'e';
+        break;
+
+      }
+      var package_image = $(`<div ><img width="128px" src="/images/packages/${i}${cat_extension}.png" alt=""></div>`);
+      var package_info = $('<ul class="package_info"></ul>');
+        package.info.map(function(info){
+          var info_item = $(`<li>${info}</li>`);
+          package_info.append(info_item);
+        });
+      var package_button = $('<button type="button" name="button">Reservar</button>');
+
+      package_div.append(package_name, [package_image, package_info, package_button]);
+      catalog_packages.append(package_div);
+      i++;
+  });
+  //Only if it has a message
+  if(catalogs_data[catalog_id].message){
+    var catalog_message = $(`<div>${catalogs_data[catalog_id].message}<div>`);
+  }
+
+  catalog.append(catalog_title, [catalog_packages, catalog_message]);
+
+
+  catalog_info.append(catalog);
+
+  //This part is for adjust the height of the package information base on how many items it has
+  var max_height = 0;
+  $('.package_info').each(function( index) {
+    if( $(this).outerHeight() > max_height){
+      max_height = $(this).outerHeight();
+    }
+  });
+  $('.package_info').css('min-height',max_height);
+
+  //Scroll to new section.
+  smoothScroll($('#catalog'));
+
+}
+
+$('.catalog_item').hover( function() {
+  $( this ).children( 'span' ).css('opacity', '1');
+  }, function() {
+  $( this ).children( 'span' ).css('opacity', '0');
+});
+
+$('.about__carousel').slick({
+  autoplay: true,
+  dots: true,
+  autoplaySpeed: 2000,
+});
